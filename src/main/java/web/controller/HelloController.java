598331d4ba2797549b1c289bef2f481a2beedf68
Lixof <lixoffs@gmail.com>
2021-03-16 00:50:35 +0300

    commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
+import web.service.CarServiceImp;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,19 +20,27 @@ public class HelloController {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
 		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
+		messages.add("5.3.4 version by mar'21 ");
 		model.addAttribute("messages", messages);
 		return "index";
 	}
 
 	@GetMapping(value = "/cars")
-	public String printCars(ModelMap model) {
+	public String printCars(ModelMap model, @RequestParam(value = "count", required = false) Integer count) {
+
 		List<Car> cars = new ArrayList<>();
-		cars.add(new Car("Evgeniy", "Peugeot", 874 ));
-		cars.add(new Car("Nikita", "Hyundai", 675 ));
-		cars.add(new Car("Natasha", "Opel", 490));
-		cars.add(new Car("Lida", "Nissan", 471 ));
-		cars.add(new Car("Slava", "Volvo", 983 ));
+		for (int i = 1 ; i < 6 ; i++) {
+			cars.add(new Car("User" + i, "Model" + i, i));
+		}
+
+		int sum = CarServiceImp.sumCars(cars);
+		if (count == null || count > sum) count = sum;
+
+		List<Car> cars1 = new ArrayList<>();
+		for (int i = 0 ; i < count ; i++){ cars1.add(cars.get(i)); }
+
+		model.addAttribute("cars", cars1);
+
 		return "cars";
 	}
 
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -5,5 +5,5 @@ import web.models.Car;
 import java.util.List;
 
 public class CarServiceImp {
-    public int sumCars(List<Car> cars) { return cars.size(); }
+    public static int sumCars(List<Car> cars) { return cars.size(); }
 }
